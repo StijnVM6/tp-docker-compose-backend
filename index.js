20b3922bfbb37f489ff99a39c3d3a2d69983e5cf
StijnVM6 <stijn.vm@hotmail.com>
2025-05-15 09:22:04 +0200

    changed path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 3001;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
 	res.json({ message: "My backend..." });
 });
 
