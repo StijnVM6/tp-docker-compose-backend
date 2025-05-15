076ac18890973de610e26e3209d39c1f53f8440c
StijnVM6 <stijn.vm@hotmail.com>
2025-05-15 10:09:06 +0200

    fix cors

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// const express = require('express');
 import express from "express";
+import cors from "cors";
+
 const app = express();
 const PORT = 3001;
+
+console.log("Starting app...");
+
+app.use(
+	cors({
+		origin: "*",
+	})
+);
 
 app.get("/api", (req, res) => {
 	res.json({ message: "My backend..." });
